c21d6b677961d3843a85c8be2a598df0033d8b5c
Eric Zorn <zornwebdev@gmail.com>
2018-11-18 12:17:13 -0500

    Remove Unecessary Imports in Task Routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,6 +1,4 @@
 const router = require("express").Router();
-const Sequelize = require("sequelize");
-const sequelize = require("../db/sequalizeSetup");
 const models = require("../models/index");
 
 router.get("/", (req, res) => {
